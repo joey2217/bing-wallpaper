00934ffc43ce3adf05b646a27024e757205f8b4a
Joey <786678257@qq.com>
2021-04-12 17:47:46 +0800

    chore:up api url

--- crawler/index.js
+++ crawler/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const fs = require('fs')
 
 const imageUrl =
-  'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1612409408851&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160'
+  'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1612409408851&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160&mkt=zh-CN'
 
 const UA =
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.81'
@@ -24,6 +24,8 @@ const BASE_URL = 'https://cn.bing.com'
       },
     })
     const wallPaperUrl = BASE_URL + url
+    // console.log(wallPaperUrl);
+    // console.log(copyright);
     const dataPath = path.resolve(__dirname, '../public/data.json')
     const dataText = fs.readFileSync(dataPath, { encoding: 'utf-8' })
     const imgList = dataText ? JSON.parse(dataText) : []
