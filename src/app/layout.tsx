a067001dacdb0c7c2d278bcd2fe7a7a46255977f
Joey <786678257@qq.com>
2024-01-22 17:01:41 +0800

    feat: baidu hm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Header from '@/components/Header'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Bing壁纸',
@@ -18,6 +19,10 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
       </body>
+      <script
+        src="https://hm.baidu.com/hm.js?c77f182c1886de6055308a84f6302ccd"
+        async
+      ></script>
     </html>
   )
 }
